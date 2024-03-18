8619403a7d65cecd71848548d1b565858582aea5
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-03-18 11:10:55 +0100

    fix(#10): delete admin-routing.module.ts and quiz-routing.module.ts, add routes into app-routing.module.ts

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
-import {AdminRoutingModule} from "./admin-routing.module";
+//import {AdminRoutingModule} from "./admin-routing.module";
 import {AdminNavbarComponent} from "./admin-navbar/admin-navbar.component";
 import {AdminComponent} from "./admin.component";
 import {LayoutModule} from "../layout/layout.module";
 import {PatientModule} from "./pages/patient/patient.module";
+import {RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -16,10 +17,10 @@ import {PatientModule} from "./pages/patient/patient.module";
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule,
     LayoutModule,
     NgOptimizedImage,
-    PatientModule
+    PatientModule,
+    RouterOutlet
   ]
 })
 export class AdminModule {
--- frontend/src/app/quiz/quiz.module.ts
+++ frontend/src/app/quiz/quiz.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {QuizPlayButtonComponent} from "./play-button/play-button.component";
-import{QuizComponent} from "./quiz.component";
+import {QuizComponent} from "./quiz.component";
 import {LayoutModule} from "../layout/layout.module";
 import {QuizHeaderComponent} from "./quiz-header/quiz-header.component";
 import {QuestionSectionComponent} from "./question-section/question-section.component";
 import {AnswersComponent} from "./answers/answers.component";
 import {RouterOutlet} from "@angular/router";
 import {QuizSectionComponent} from "./quiz-section/quiz-section.component";
-import {QuizRoutingModule} from "./quiz-routing.module";
+//import {QuizRoutingModule} from "./quiz-routing.module";
 import {IntermediatePageComponent} from "./intermediate-page/intermediate-page.component";
 import {FinishPageComponent} from "./finish/finish-page.component";
 
@@ -28,7 +28,6 @@ import {FinishPageComponent} from "./finish/finish-page.component";
     NgOptimizedImage,
     LayoutModule,
     RouterOutlet,
-    QuizRoutingModule
   ]
 })
 export class QuizModule {
