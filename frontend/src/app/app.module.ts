06a7583135a7d599d1eadb6aa57a035ee8b8dd91
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-03-19 20:10:08 +0100

    fix(#12): Correct bug after previous commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {NgOptimizedImage} from "@angular/common";
+import {AdminModule} from "./admin/admin.module";
 import {QuizModule} from "./quiz/quiz.module";
 
 @NgModule({
@@ -14,6 +14,7 @@ import {QuizModule} from "./quiz/quiz.module";
     BrowserModule,
     AppRoutingModule,
     NgOptimizedImage,
+    AdminModule,
     QuizModule
   ],
   providers: [],
