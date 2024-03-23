33a3e0ce3702e749ac8e4eac6ada2f0f222915b5
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2024-03-23 11:27:01 +0100

    refactor(#15): Code style

--- frontend/src/app/quiz/quiz-section/quiz-section.component.ts
+++ frontend/src/app/quiz/quiz-section/quiz-section.component.ts
@@ -10,7 +10,7 @@ import {Question} from "../../../models/question.models";
   styleUrls: ['./quiz-section.component.scss']
 })
 export class QuizSectionComponent {
-  protected question:Question | undefined;
+  protected question?: Question;
 
   constructor(private router: Router, private route: ActivatedRoute, public gameService: GameService) {
     this.gameService.question$.subscribe((question) => {
@@ -18,27 +18,27 @@ export class QuizSectionComponent {
     });
   }
 
-  checkAnswer(answer:String): void{
-    if(this.question?.trueAnswer == answer) //Open the intermediate page with good text depends on answer (if it is good or not)
+  checkAnswer(answer: String): void {
+    if (this.question?.trueAnswer == answer) //Open the intermediate page with good text depends on answer (if it is good or not)
       this.router.navigate(["../intermediate", true], {relativeTo: this.route}).then(
         r => {
-          if(r)this.continueQuiz();
+          if (r) this.continueQuiz();
           else console.log("Quiz intermediate launch error")
         })
     else
       this.router.navigate(["../intermediate", false], {relativeTo: this.route}).then(
         r => {
-          if(r)this.continueQuiz();
+          if (r) this.continueQuiz();
           else console.log("Quiz intermediate launch error")
         })
   }
 
-  continueQuiz(){
-      this.gameService.nextQuestion();
-      if(this.question == undefined) //Open the finish page at the end of the quiz
-        this.router.navigate(["../finish"], {relativeTo: this.route}).then(
-          r => {
-            if(!r)console.log("Quiz finish launch error")
-          })
-    }
+  continueQuiz() {
+    this.gameService.nextQuestion();
+    if (!this.question) //Open the finish page at the end of the quiz
+      this.router.navigate(["../finish"], {relativeTo: this.route}).then(
+        r => {
+          if (!r) console.log("Quiz finish launch error")
+        })
+  }
 }
