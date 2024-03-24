d441504d6210476985cd6a3999d203643d4b82a5
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-03-24 22:32:44 +0100

    fix(#4) : fixed a compilation error

--- frontend/src/app/quiz/quiz.component.ts
+++ frontend/src/app/quiz/quiz.component.ts
@@ -14,6 +14,6 @@ export class QuizComponent  implements OnInit{
   }
 
   ngOnInit(): void {
-    this.user = this.userService.getCurrentUser();
+    this.userService.user$.subscribe(user => this.user = user);
   }
 }
