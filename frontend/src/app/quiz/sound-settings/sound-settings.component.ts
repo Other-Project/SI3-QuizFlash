07739d3f96e5fff87ddf31653b8e2fef22255e96
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-03-24 22:10:34 +0100

    feature(#6): replace document.getElementsByTagName by @ViewChild

--- frontend/src/app/quiz/sound-settings/sound-settings.component.ts
+++ frontend/src/app/quiz/sound-settings/sound-settings.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from "@angular/core";
+import {AfterViewInit, Component, ElementRef, ViewChild} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
 import {QuizService} from "../../../service/quiz-service.service";
 import {QuestionType} from "../../../models/question-type.models";
@@ -10,8 +10,8 @@ import {UserService} from "../../../service/user.service";
   styleUrls: ['./sound-settings.component.scss']
 })
 
-export class SoundSettingsComponent implements OnInit {
-  private audio: HTMLMediaElement | null = null;
+export class SoundSettingsComponent implements AfterViewInit {
+  @ViewChild("sound") audio?: ElementRef;
   private audioContext: AudioContext = new AudioContext();
   private soundNode?: MediaElementAudioSourceNode;
   private gainNode: GainNode = this.audioContext.createGain();
@@ -30,24 +30,23 @@ export class SoundSettingsComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
-    this.audio = document.getElementsByTagName("audio").item(0);
+  ngAfterViewInit(): void {
     if (this.audio) {
-      this.soundNode = this.audioContext.createMediaElementSource(this.audio);
+      this.soundNode = this.audioContext.createMediaElementSource(this.audio.nativeElement);
       this.gainNode.gain.value = this.gainValue;
       this.soundNode.connect(this.gainNode).connect(this.audioContext.destination);
     }
   }
 
   play() {
-    if (!this.audio || this.audio.readyState < 2) return;
-    this.audio.play().then();
+    if (!this.audio || this.audio.nativeElement.readyState < 2) return;
+    this.audio.nativeElement.play().then();
     this.soundPlayed = true;
   }
 
   pause() {
     if (!this.audio) return;
-    this.audio.pause();
+    this.audio.nativeElement.pause();
     this.soundPlayed = false;
   }
 
