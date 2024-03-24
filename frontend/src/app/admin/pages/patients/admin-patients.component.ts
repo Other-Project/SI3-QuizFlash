c4c40f4239681ebe53db26001e1bb8a374eb4fea
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2024-03-24 23:47:20 +0100

    feat(#9): Admin patient list

--- frontend/src/app/admin/pages/patients/admin-patients.component.ts
+++ frontend/src/app/admin/pages/patients/admin-patients.component.ts
@@ -1,12 +1,25 @@
-import {Component} from '@angular/core';
+import {Component} from "@angular/core";
+import {UserService} from "../../../../service/user.service";
+import {User} from "../../../../models/user.models";
+import {ProfileListComponent} from "../../../profiles/profile-list/profile-list.component";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
-  selector: 'app-admin-patients',
-  templateUrl: './admin-patients.component.html',
+  selector: "app-admin-patients",
+  templateUrl: "./admin-patients.component.html",
   standalone: true,
-  //styleUrls: ['./admin-patients.component.scss']
+  imports: [
+    ProfileListComponent
+  ]
 })
 export class AdminPatientsComponent {
-  constructor() {
+  public users?: User[];
+
+  constructor(userService: UserService, private router: Router, private route: ActivatedRoute) {
+    userService.users$.subscribe(users => this.users = users);
+  }
+
+  openUser(user: User) {
+    this.router.navigate(["../patient", user.id], {relativeTo: this.route}).then();
   }
 }
