b5ab0579053c3eec642f5c85f51936f342cc3d41
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-03-24 20:57:45 +0100

    feature(#6): refactor UserService

--- frontend/src/service/user.service.ts
+++ frontend/src/service/user.service.ts
@@ -3,14 +3,13 @@ import {BehaviorSubject} from "rxjs";
 import {User} from "../models/user.models";
 import {USERS} from "../mocks/users.mock";
 import {USER_BERNARD} from "../mocks/user-bernard.mock";
-import {USER_MARTINE} from "../mocks/user-martine.mock";
 
 @Injectable({providedIn: 'root'})
 export class UserService {
   public users: User[] = USERS;
   public users$: BehaviorSubject<User[]> = new BehaviorSubject<User[]>(this.users);
   public user?: User;
-  public user$: BehaviorSubject<User | undefined> = new BehaviorSubject<User | undefined>(USER_MARTINE);
+  public user$: BehaviorSubject<User | undefined> = new BehaviorSubject<User | undefined>(this.user);
 
   constructor() {
   }
