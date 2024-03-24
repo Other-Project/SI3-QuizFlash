a6088ed61c3065847fabcf81075082151eeed1e1
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2024-03-24 23:47:43 +0100

    feat(#9): Redirect root admin page to the patient list

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
 import {AdminComponent} from "./admin/admin.component";
 import {QuizComponent} from "./quiz/quiz.component";
 import {QuizSectionComponent} from "./quiz/quiz-section/quiz-section.component";
@@ -15,31 +15,32 @@ import {ProfilesComponent} from "./profiles/profiles.component";
 
 
 const routes: Routes = [
-  {path: '', pathMatch: "full", component:ProfilesComponent},
+  {path: "", pathMatch: "full", component: ProfilesComponent},
   {
-    path: 'admin', component: AdminComponent, children: [
-      {path: 'patients', component: AdminPatientsComponent},
+    path: "admin", component: AdminComponent, children: [
+      {path: "", pathMatch: "full", redirectTo: "patients"},
+      {path: "patients", component: AdminPatientsComponent},
       {
-        path: 'patient/:user_id', component: PatientComponent, children: [
-          {path: '', pathMatch: 'full', redirectTo: 'infos'},
-          {path: 'infos', component: InformationComponent},
-          {path: 'stats', component: StatisticsComponent}
+        path: "patient/:user_id", component: PatientComponent, children: [
+          {path: "", pathMatch: "full", redirectTo: "infos"},
+          {path: "infos", component: InformationComponent},
+          {path: "stats", component: StatisticsComponent}
         ]
       },
-      {path: 'quizz', component: AdminQuizzComponent}
+      {path: "quizz", component: AdminQuizzComponent}
     ]
   },
   {
-    path: 'quiz', component: QuizComponent, children: [
-      {path: '', pathMatch: 'full', component: QuizSelectionComponent}
+    path: "quiz", component: QuizComponent, children: [
+      {path: "", pathMatch: "full", component: QuizSelectionComponent}
     ]
   },
   {
-    path: 'quiz/:quiz_id', component: QuizComponent, children: [
-      {path: '', pathMatch: 'full', redirectTo: 'sound-settings'},
-      {path: 'sound-settings', component: SoundSettingsComponent},
-      {path: 'question', component: QuizSectionComponent},
-      {path: 'finish', component: FinishPageComponent}
+    path: "quiz/:quiz_id", component: QuizComponent, children: [
+      {path: "", pathMatch: "full", redirectTo: "sound-settings"},
+      {path: "sound-settings", component: SoundSettingsComponent},
+      {path: "question", component: QuizSectionComponent},
+      {path: "finish", component: FinishPageComponent}
     ]
   }
 ];
