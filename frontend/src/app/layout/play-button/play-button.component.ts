1c5c730a0ef674c2ba166d5d157ef19454f25d34
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-03-25 10:34:17 +0100

    fix(#26) : The sound can be played by staying on the page (didn't work previously)

--- frontend/src/app/layout/play-button/play-button.component.ts
+++ frontend/src/app/layout/play-button/play-button.component.ts
@@ -11,25 +11,27 @@ import {NgIf, NgOptimizedImage} from "@angular/common";
   ],
   standalone: true
 })
-export class PlayButtonComponent implements AfterViewInit{
+export class PlayButtonComponent {
   @Input() public src?: string;
   @Input() set volume(gainValue: number) {
     this.gainValue = gainValue;
-    this.gainNode.gain.value = this.gainValue;
-    console.log(gainValue);
+    if (this.gainNode)
+      this.gainNode.gain.value = this.gainValue;
   }
 
   @ViewChild("sound") audio?: ElementRef;
-  private audioContext: AudioContext = new AudioContext();
+  private audioContext?: AudioContext;
   private soundNode?: MediaElementAudioSourceNode;
-  private gainNode: GainNode = this.audioContext.createGain();
+  private gainNode?: GainNode;
   private gainValue: number = 2;
   protected soundPlayed: boolean = false;
   constructor() {
   }
 
-  ngAfterViewInit(): void {
-    if (this.audio) {
+  init(): void {
+    if (this.audio && !this.audioContext) {
+      this.audioContext = new AudioContext();
+      this.gainNode = this.audioContext.createGain();
       this.soundNode = this.audioContext.createMediaElementSource(this.audio.nativeElement);
       this.gainNode.gain.value = this.gainValue;
       this.soundNode.connect(this.gainNode).connect(this.audioContext.destination);
@@ -37,6 +39,7 @@ export class PlayButtonComponent implements AfterViewInit{
   }
 
   play() {
+    this.init();
     if (!this.audio || this.audio.nativeElement.readyState < 2) return;
     this.audio.nativeElement.play().then();
     this.soundPlayed = true;
