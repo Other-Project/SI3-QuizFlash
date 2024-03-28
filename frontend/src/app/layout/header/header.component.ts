b6636e2818347e2ead8a83562525360c254bb7dd
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2024-03-28 17:57:53 +0100

    feat(#25): Click on logo to go the home page + Logout button

--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -1,11 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, Input} from "@angular/core";
+import {faSignOut} from "@fortawesome/free-solid-svg-icons";
+import {UserService} from "../../../service/user.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent {
-  constructor() {
+  @Input() homePage?: any[] | string;
+  @Input() interactive: boolean = true;
+
+  loggedIn: boolean = false;
+
+  constructor(private userService: UserService) {
+    userService.user$.subscribe(user => this.loggedIn = user != undefined);
   }
+
+  public logout() {
+    if (!this.loggedIn) return;
+    this.userService.setLoggedUser();
+  }
+
+  protected readonly faSignOut = faSignOut;
 }
