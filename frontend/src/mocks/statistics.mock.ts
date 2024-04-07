01a61cee6f8661f10b0bfa25b22936570bfc0f98
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2024-04-07 13:14:17 +0200

    feat(#39): Adding a second try in mock

--- frontend/src/mocks/statistics.mock.ts
+++ frontend/src/mocks/statistics.mock.ts
@@ -27,11 +27,42 @@ export const QUESTIONSTAT3: QuestionStats = {
   assistedAnswer: false
 }
 
+export const QUESTIONSTAT1TRY2: QuestionStats = {
+  questionId: "1",
+  questionType: QuestionType.TextOnly,
+  success: false,
+  timeSpent: 2,
+  assistedAnswer: false
+}
+
+export const QUESTIONSTAT2TRY2: QuestionStats = {
+  questionId: "2",
+  questionType: QuestionType.TextOnly,
+  success: true,
+  timeSpent: 2.8,
+  assistedAnswer: false
+}
+
+export const QUESTIONSTAT3TRY2: QuestionStats = {
+  questionId: "3",
+  questionType: QuestionType.Sound,
+  success: true,
+  timeSpent: 3,
+  assistedAnswer: false
+}
+
 export const QUIZSTATS1: QuizStats = {
   userId: "134b849f-fd4b-4b13-b377-f283b117365d",
   quizId: "1",
 
   questionsStats: [QUESTIONSTAT1, QUESTIONSTAT2, QUESTIONSTAT3]
+}
+
+export const QUIZSTATS1TRY2: QuizStats = {
+  userId: "134b849f-fd4b-4b13-b377-f283b117365d",
+  quizId: "1",
+
+  questionsStats: [QUESTIONSTAT1TRY2, QUESTIONSTAT2TRY2, QUESTIONSTAT3TRY2]
 }
 
 export const QUIZSTATS2: QuizStats = {
@@ -43,5 +74,5 @@ export const QUIZSTATS2: QuizStats = {
 
 
 export const STATISTICS: QuizStats[] = [
-  QUIZSTATS1, QUIZSTATS2
+  QUIZSTATS1, QUIZSTATS2, QUIZSTATS1TRY2
 ]
