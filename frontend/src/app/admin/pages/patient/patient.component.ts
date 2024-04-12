b74860b0e5d344516bb27f298a71fcbbf8669baa
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-04-12 14:11:59 +0200

    feat(#49) : On refresh, we root on admin/patient if we were on a created user

--- frontend/src/app/admin/pages/patient/patient.component.ts
+++ frontend/src/app/admin/pages/patient/patient.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {UserService} from "../../../../service/user.service";
 import {User} from "../../../../models/user.models";
 import {Patient} from "../../../../models/patient.models";
@@ -20,12 +20,19 @@ export class PatientComponent {
   public user?: Patient;
   public tab: TabNavigation = TabNavigation.INFORMATION;
 
-  constructor(private route: ActivatedRoute, private userService: UserService) {
+  constructor(private route: ActivatedRoute, private router: Router, private userService: UserService) {
     this.route.params.subscribe(params => {
       let user_id: string = params["user_id"];
-      this.userService.users$.subscribe(users => this.user = users.find(user => user.id == user_id) as Patient);
+      if (!user_id) this.user = undefined;
+      else {
+        this.userService.users$.subscribe(users => {
+          this.user = users.find(user => user.id == user_id) as Patient;
+          if (!this.user) this.router.navigate([".."], {relativeTo: this.route}).then();
+        });
+      }
     });
   }
+
 
   protected readonly TabNavigation = TabNavigation;
 }
