5fbfd230d0ef5fb59099362f607899cf7d8522af
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2024-04-14 10:06:26 +0200

    fix(#39): Fix merge bugs

--- frontend/src/app/admin/pages/patient/statistics/statistics.component.ts
+++ frontend/src/app/admin/pages/patient/statistics/statistics.component.ts
@@ -19,13 +19,13 @@ export class StatisticsComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.route.parent?.params.subscribe(params => {
+    this.route.params.subscribe(params => {
       let user_id: string = params['user_id'];
       this.user = this.userService.getUserById(user_id) as Patient;
     });
   }
 
   quizSelection(quizSelectionData: { quizId?: string, questionType?: QuestionType }) {
-    this.graph.quizSelection(quizSelectionData.quizId, quizSelectionData.questionType);
+    this.graph?.quizSelection(quizSelectionData.quizId, quizSelectionData.questionType);
   }
 }
