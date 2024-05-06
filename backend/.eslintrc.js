46ae0705cad0631f1906fc5e14ada863343deca8
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2024-05-06 10:12:43 +0200

    style(#71): ESLint configuration

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
-  extends: 'airbnb-base',
-  env: {
-    jest: true,
-    node: true,
-  },
-  rules: {
-    'max-len': ['error', 200, { 'ignoreStrings': true }],
-    'no-underscore-dangle': ['error', { 'allow': ['_id'] }],
-    'semi': ['error', 'never'],
-  }
+    extends: "airbnb-base",
+    env: {
+        jest: true,
+        node: true
+    },
+    rules: {
+        "max-len": ["warn", 200, { "ignoreStrings": true }],
+        "no-underscore-dangle": ["warn", { "allow": ["_id"] }],
+        "comma-dangle": ["warn", "never"],
+        "semi": ["warn", "always"],
+        "indent": ["warn", 4],
+        "quotes": ["warn", "double"]
+    }
 };
