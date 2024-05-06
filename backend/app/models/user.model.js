23094e858fe9b24180cae4988973ced601b3ea0c
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-05-06 10:25:48 +0200

    feat(#70) : User model is now the same as the one in the front

--- backend/app/models/user.model.js
+++ backend/app/models/user.model.js
@@ -2,6 +2,10 @@ const Joi = require("joi");
 const BaseModel = require("../utils/base-model.js");
 
 module.exports = new BaseModel("User", {
+    id: Joi.string().required(),
+    lastName: Joi.string().required(),
     firstName: Joi.string().required(),
-    lastName: Joi.string().required()
+    age: Joi.number().required(),
+    pictureUrl: Joi.string().required()
+    /* TODO add access restriction */
 });
