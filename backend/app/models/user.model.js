7c080273eb22861fa294286c220b654f88d298ef
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-05-06 10:48:01 +0200

    feat(#70) : Added access restriction param in user model

--- backend/app/models/user.model.js
+++ backend/app/models/user.model.js
@@ -3,9 +3,9 @@ const BaseModel = require("../utils/base-model.js");
 
 module.exports = new BaseModel("User", {
     id: Joi.string().required(),
+    accessRestriction: Joi.string().required,
     lastName: Joi.string().required(),
     firstName: Joi.string().required(),
     age: Joi.number().required(),
     pictureUrl: Joi.string().required()
-    /* TODO add access restriction */
 });
