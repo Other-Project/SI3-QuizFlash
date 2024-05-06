ad7472f0b51a86ebdeddd0aa65d1a43cf58c7ee7
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-05-06 11:00:37 +0200

    refactor(#70) : Limited range of certain parameters of user model

--- backend/app/models/user.model.js
+++ backend/app/models/user.model.js
@@ -2,10 +2,10 @@ const Joi = require("joi");
 const BaseModel = require("../utils/base-model.js");
 
 module.exports = new BaseModel("User", {
-    id: Joi.string().required(),
-    accessRestriction: Joi.string().required,
+    id: Joi.string().guid().required(),
+    accessRestriction: Joi.string().valid("Guest", "User", "Admin").required(),
     lastName: Joi.string().required(),
     firstName: Joi.string().required(),
-    age: Joi.number().required(),
+    age: Joi.number().min(0).required(),
     pictureUrl: Joi.string().required()
 });
