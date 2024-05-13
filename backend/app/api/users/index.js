48edd71d7f3d243f7e1969fd8be9ff0ab9c1e60e
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2024-05-13 11:34:02 +0200

    doc(#81): Users OpenAPI description

--- backend/app/api/users/index.js
+++ backend/app/api/users/index.js
@@ -1,50 +1,79 @@
-const {Router} = require("express");
+const { Router } = require("express");
 
-const {User} = require("../../models");
-const manageAllErrors = require("../../utils/routes/error-management");
+const { User } = require("../../models");
+const { catchErrors } = require("../../utils/errors/routes");
 
 const router = new Router();
 
-router.get("/", (req, res) => {
-    try {
-        res.status(200).json(User.get());
-    } catch (err) {
-        manageAllErrors(res, err);
-    }
-});
+router.get("/", (req, res) => catchErrors(req, res, () => {
+    /*  #swagger.tags = ['Users']
+        #swagger.summary = 'Get all users'
+        #swagger.responses[200] = {
+          schema: [{ $ref: '#/definitions/User' }]
+        } */
+    res.status(200).json(User.get());
+}));
 
-router.get("/:userId", (req, res) => {
-    try {
-        res.status(200).json(User.getById(req.params.userId));
-    } catch (err) {
-        manageAllErrors(res, err);
-    }
-});
+router.get("/:userId", (req, res) => catchErrors(req, res, () => {
+    /*  #swagger.tags = ['Users']
+        #swagger.summary = 'Get a specific user'
+        #swagger.responses[200] = {
+          schema: { $ref: '#/definitions/User' }
+        }
+        #swagger.responses[404] = {
+              description: 'No user found with this id'
+        } */
+    res.status(200).json(User.getById(req.params.userId));
+}));
 
-router.post("/", (req, res) => {
-    try {
-        const user = User.create({...req.body});
-        res.status(201).json(user);
-    } catch (err) {
-        manageAllErrors(res, err);
-    }
-});
+router.post("/", (req, res) => catchErrors(req, res, () => {
+    /*  #swagger.tags = ['Users']
+        #swagger.summary = 'Add new user'
+        #swagger.parameters['body'] = {
+            in: 'body',
+            schema: { $ref: '#/definitions/User' }
+        }
+        #swagger.responses[201] = {
+          schema: { $ref: '#/definitions/User' }
+        }
+        #swagger.responses[400] = {
+              description: 'Invalid request'
+        } */
 
-router.put("/:userId", (req, res) => {
-    try {
-        res.status(200).json(User.update(req.params.userId, req.body));
-    } catch (err) {
-        manageAllErrors(res, err);
-    }
-});
+    const user = User.create({ ...req.body });
+    res.status(201).json(user);
+}));
 
-router.delete("/:userId", (req, res) => {
-    try {
-        User.delete(req.params.userId);
-        res.status(204).end();
-    } catch (err) {
-        manageAllErrors(res, err);
-    }
-});
+router.put("/:userId", (req, res) => catchErrors(req, res, () => {
+    /*  #swagger.tags = ['Users']
+        #swagger.summary = 'Modify an existing user'
+        #swagger.parameters['body'] = {
+            in: 'body',
+            schema: { $ref: '#/definitions/User' }
+        }
+        #swagger.responses[200] = {
+          schema: { $ref: '#/definitions/User' }
+        }
+        #swagger.responses[400] = {
+              description: 'Invalid request'
+        }
+        #swagger.responses[404] = {
+              description: 'No user found with this id'
+        } */
+
+    res.status(200).json(User.update(req.params.userId, req.body));
+}));
+
+router.delete("/:userId", (req, res) => catchErrors(req, res, () => {
+    /*  #swagger.tags = ['Users']
+        #swagger.summary = 'Delete an user'
+        #swagger.responses[204] = { }
+        #swagger.responses[404] = {
+              description: 'No user found with this id'
+        } */
+
+    User.delete(req.params.userId);
+    res.status(204).end();
+}));
 
 module.exports = router;
