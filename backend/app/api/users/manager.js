fd6831147a0031d6718ed02b76ab1c21964903c7
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-05-27 10:54:53 +0200

    style(#79) : cleaned up user manager

--- backend/app/api/users/manager.js
+++ backend/app/api/users/manager.js
@@ -10,30 +10,6 @@ function createUser(user) {
     return User.create(user);
 }
 
-function replaceUser(userId, user) {
-    /* const {questions, ...pureQuiz} = quiz;
-    let result = Quiz.replace(quizId, pureQuiz);
-    let currentQuestions = getQuizQuestions(quiz.id);
-    currentQuestions.filter(question => questions.every(q => q.id !== question.id)).forEach(question => Question.delete(question.id));
-    result.questions = questions.map(question => question.id ? replaceQuestion(question.id, question) : createQuestion(quiz.id, question));
-    return result;
-     */
-}
-
-function updateUser(userId, user) {
-    /* const {questions, ...pureQuiz} = quiz;
-    let result = Quiz.update(quizId, pureQuiz);
-    result.questions = questions.map(question => question.id ? updateQuestion(question.id, question) : createQuestion(quiz.id, question));
-    return result;
-     */
-}
-
-function deleteUser(userId) {
-    /* getQuizQuestions(quizId).forEach(question => deleteQuestion(question.id));
-    Quiz.delete(quizId);
-     */
-}
-
 module.exports = {
     createUser
 };
