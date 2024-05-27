059d8811afd470766b7fc85d7cdca2c56b45ea49
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2024-05-27 09:40:00 +0200

    feat(#76): Improvement of input's check

--- frontend/src/app/admin/pages/patient/quantity-input/quantity-input.component.ts
+++ frontend/src/app/admin/pages/patient/quantity-input/quantity-input.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {Component, ElementRef, EventEmitter, Input, Output, ViewChild} from "@angular/core";
 
 @Component({
   selector: "quantity-input",
@@ -7,6 +7,7 @@ import {Component, EventEmitter, Input, Output} from "@angular/core";
 })
 
 export class QuantityInputComponent {
+  @ViewChild("valueInput") valueInput!: ElementRef;
   @Input() label!: string;
   @Input() min: number = -Infinity;
   @Input() max: number = Infinity;
@@ -27,7 +28,21 @@ export class QuantityInputComponent {
     }
   }
 
-  onChange(): void {
-    this.valueChange.emit(this.value);
+  onChange(newVal: number): void {
+    console.log(newVal);
+    if (newVal > this.min && newVal < this.max)
+      this.valueChange.emit(this.value);
+  }
+
+  checkValue(): void {
+    let val = this.valueInput.nativeElement.value;
+    let isNegative = this.min < 0 ? val.charAt(0) === "-" : false;
+    val = val.replace(/^0+|\D/g, "");
+    let refValue = (isNegative) ? Math.abs(this.min) : this.max;
+    if (parseInt(val) > refValue)
+      val = val.substring(0, val.toString().length - 1);
+    if (isNegative)
+      val = "-" + val;
+    this.valueInput.nativeElement.value = val;
   }
 }
