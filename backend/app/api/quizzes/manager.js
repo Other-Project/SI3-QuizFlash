7a23bf15ce21697221c71ef14e21f6000fe73262
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-05-27 11:23:02 +0200

    feat(#89): add user quiz adaptation

--- backend/app/api/quizzes/manager.js
+++ backend/app/api/quizzes/manager.js
@@ -6,14 +6,21 @@ const { buildStat } = require("../statistics/manager");
 /**
  * This function aggregates the questions and answers from the database to build a quiz with all the data needed by the clients
  * @param {string|number} quizId
+ * @param {string|number|undefined} userId
  */
-function buildQuiz(quizId) {
+function buildQuiz(quizId, userId) {
     const quiz = Quiz.getById(quizId);
     const questions = getQuizQuestions(quiz.id);
     const questionWithAnswers = questions.map((question) => {
-        const answers = getQuestionAnswers(question.id);
+        let answers = getQuestionAnswers(question.id);
+        if (userId !== undefined) {
+            answers.map(answer => delete answer.trueAnswer);
+        }
         return {...question, answers};
     });
+    if (userId !== undefined) {
+
+    }
     return {...quiz, questions: questionWithAnswers};
 }
 
