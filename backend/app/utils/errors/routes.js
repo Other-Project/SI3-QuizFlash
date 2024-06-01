e5276c55fd4d8caaf1c8353480535724e71d01fe
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2024-06-01 15:44:22 +0200

    feat(#90): Improved error logging

--- backend/app/utils/errors/routes.js
+++ backend/app/utils/errors/routes.js
@@ -1,6 +1,7 @@
 function manageAllErrors(res, err) {
-    let resBuilder = res.status(err.code ?? 500);
+    let resBuilder = res.status((err.code || err.status) ?? 500);
     if (err.extra) resBuilder.json(err.extra);
+    else if (err.message || err.stack) resBuilder.json({message: err.message, stack_trace: err.stack.split("\n").map(line => line.replace(/^\s+/g, ""))});
     else resBuilder.end();
 }
 
