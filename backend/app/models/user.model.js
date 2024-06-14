11608cac244868923247616bf48e2f48652cba7a
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2024-06-14 19:37:04 +0200

    refactor(#112): Update of the models with the replacement of age by birthDate

--- backend/app/models/user.model.js
+++ backend/app/models/user.model.js
@@ -7,10 +7,14 @@ module.exports = new BaseModel("User", {
     access: Joi.number().valid(Object.values(AccessRestriction)).required(),
     lastname: Joi.string().required(),
     firstname: Joi.string().required(),
-    age: Joi.number().positive().required(),
     pictureUrl: Joi.string().allow("").uri(),
 
     //Patient attributes
+    birthDate: Joi.date()
+        .required()
+        .max(getMaxDate())
+        .min(getMinDate())
+        .when("access", { is: AccessRestriction.user, otherwise: Joi.forbidden() }),
     hobbies: Joi.array().items(Joi.string()).when("access", { is: AccessRestriction.user, otherwise: Joi.forbidden() }),
     dementiaLevel: Joi.number().valid(Object.values(DementiaLevel))
         .when("access", { is: AccessRestriction.user, otherwise: Joi.forbidden() }),
@@ -27,3 +31,18 @@ module.exports = new BaseModel("User", {
     password: Joi.string().when("access", {is: AccessRestriction.admin, then: Joi.required(), otherwise: Joi.forbidden()}),
     salt: Joi.string().when("access", {is: AccessRestriction.admin, then: Joi.required(), otherwise: Joi.forbidden()})
 });
+
+function getDate(offsetYears) {
+    const date = new Date();
+    date.setFullYear(date.getFullYear() + offsetYears);
+    date.setHours(0, 0, 0);
+    return date;
+}
+
+function getMaxDate() {
+    return getDate(-1);
+}
+
+function getMinDate() {
+    return getDate(-120);
+}
