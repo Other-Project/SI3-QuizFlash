6279d5c23728b53b7d8843829338a3032b4145b8
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-06-19 10:06:44 +0200

    feat(#142): Added errors cases to the test

--- frontend/e2e/scenarios/create-user-error-test.spec.ts
+++ frontend/e2e/scenarios/create-user-error-test.spec.ts
@@ -43,7 +43,7 @@ test.describe("Create user with errors test display", () => {
       await expect(validateButton).toBeVisible();
     });
 
-    await test.step("Create User with errors in name and firstname", async () => {
+    await test.step("Create User with errors in name and firstname 1", async () => {
       await profilePictureInput.setInputFiles(["./src/assets/users/bernard.jpg"]);
       await lastNameInput.fill("12345");
       await firstNameInput.fill("12345");
@@ -53,13 +53,52 @@ test.describe("Create user with errors test display", () => {
       await expect(page).toHaveURL(`${testUrl}/admin/patient`);
     });
 
-    await test.step("Create User with errors in name and firstname", async () => {
+    await test.step("Create User with errors in name and firstname 2", async () => {
+      // Added a dash at the beginning
+      await lastNameInput.fill("-Dupont");
+      await firstNameInput.fill("Bernard");
+      await birthDateInput.fill("1950-05-05");
+      await validateButton.click();
+      // We shouldn't be able to create such a user
+      await expect(page).toHaveURL(`${testUrl}/admin/patient`);
+    });
+
+    await test.step("Create User with errors in name and firstname 3", async () => {
+      await lastNameInput.fill("Dupont");
+      // Added a space at the end
+      await firstNameInput.fill("Bernard ");
+      await birthDateInput.fill("1950-05-05");
+      await validateButton.click();
+      // We shouldn't be able to create such a user
+      await expect(page).toHaveURL(`${testUrl}/admin/patient`);
+    });
+
+    await test.step("Create User with error in birthdate", async () => {
       await lastNameInput.fill("Dupont");
       await firstNameInput.fill("Bernard");
       await birthDateInput.fill("1800-05-05");
       await validateButton.click();
       // We shouldn't be able to create such a user
       await expect(page).toHaveURL(`${testUrl}/admin/patient`);
+    });
+
+    await test.step("Create User without errors", async () => {
+      await lastNameInput.fill("Dupont");
+      await firstNameInput.fill("Jean-Marie");
+      await birthDateInput.fill("1950-05-05");
+      await validateButton.click();
+      // We should be able to create such a user
+      const patientSettingsFixture = patientFixture.getPatientSettings();
+      const dementiaInput = patientSettingsFixture.getDementiaLevel();
+      const fontSizeInput = patientSettingsFixture.getFontSize();
+      const removeWrongAnswersInput = patientSettingsFixture.getRemoveWrongAnswers();
+      const fiftyFiftyInput = patientSettingsFixture.getFiftyFifty();
+      const audioQuestionsInput = patientSettingsFixture.getAudioQuestions();
+      await expect(dementiaInput).toBeVisible();
+      await expect(fontSizeInput).toBeVisible();
+      await expect(removeWrongAnswersInput).toBeVisible();
+      await expect(fiftyFiftyInput).toBeVisible();
+      await expect(audioQuestionsInput).toBeVisible();
     });
   });
 });
