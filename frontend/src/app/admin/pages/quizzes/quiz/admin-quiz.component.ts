5de394fe9a7efcf2368259205c97670b2127159c
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-06-20 14:18:44 +0200

    feat(#154): add requested changes

--- frontend/src/app/admin/pages/quizzes/quiz/admin-quiz.component.ts
+++ frontend/src/app/admin/pages/quizzes/quiz/admin-quiz.component.ts
@@ -80,7 +80,7 @@ export class AdminQuizComponent implements OnDestroy {
     if (!this.quizForm.valid) return;
     if (this.quiz.id) {
       this.quizService.updateQuiz(this.quiz.id, this.quizForm.value).catch(() => {
-        alert("Il y a eu une erreur lors de la récupération des différents hobbies.\nVeuillez recharger la page");
+        alert("Il y a eu une erreur lors de la récupération des différents centres d'intérêt.\nVeuillez recharger la page");
       }).finally(() => this.loading = false);
       return;
     }
