7cb326f99738fbd1c49a8c968ac84c2b79f3355f
Antoine-Marie Michelozzi <antoine-marie.michelozzi@etu.unice.fr>
2024-06-20 15:27:17 +0200

    feat(#151): Taking into account addition of genders

--- frontend/src/utils/profile-picture.utils.ts
+++ frontend/src/utils/profile-picture.utils.ts
@@ -2,8 +2,14 @@ import {Genders} from "../models/genders.model";
 import {faBug, faMars, faVenus} from "@fortawesome/free-solid-svg-icons";
 
 export function getDefaultProfilePicture(gender: Genders | undefined) {
-  return (!gender || gender == Genders.MALE) ? "/assets/male-profile-picture.png"
-    : "/assets/female-profile-picture.webp";
+  switch (gender) {
+    case Genders.MALE:
+      return "/assets/male-profile-picture.png";
+    case Genders.FEMALE:
+      return "/assets/female-profile-picture.webp";
+    default:
+      return "/assets/default-profile-picture.jpg";
+  }
 }
 
 export function getFaIcon(gender: Genders) {
