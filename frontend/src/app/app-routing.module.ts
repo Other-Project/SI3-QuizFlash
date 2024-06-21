f43f493f557654ccdfb0e380df910e154853e713
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2024-06-21 16:04:25 +0200

    refactor(#175): Remove access-checker

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import {inject, NgModule} from "@angular/core";
+import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {AdminComponent} from "./admin/admin.component";
 import {QuizGameComponent} from "./quiz/quiz-game/quiz-game.component";
@@ -6,17 +6,13 @@ import {AdminPatientsComponent} from "./admin/pages/patients/admin-patients.comp
 import {PatientComponent} from "./admin/pages/patient/patient.component";
 import {AdminQuizzesComponent} from "./admin/pages/quizzes/admin-quizzes.component";
 import {ProfilesComponent} from "./profiles/profiles.component";
-import {AccessChecker} from "../service/access-checker.service";
-import {AccessRestriction} from "../models/access-restriction.models";
 import {AdminQuizComponent} from "./admin/pages/quizzes/quiz/admin-quiz.component";
 import {QuizGameSelectionComponent} from "./quiz/quiz-game-selection/quiz-game-selection.component";
-
-export const checkAccess = (restriction: AccessRestriction) => inject(AccessChecker).canActivate(restriction);
 
 const routes: Routes = [
   {path: "", pathMatch: "full", component: ProfilesComponent},
   {
-    path: "admin", component: AdminComponent, canActivate: [() => checkAccess(AccessRestriction.Admin)], children: [
+    path: "admin", component: AdminComponent, children: [
       {path: "", pathMatch: "full", redirectTo: "patients"},
       {path: "patients", component: AdminPatientsComponent},
       {path: "patient", component: PatientComponent},
@@ -31,7 +27,7 @@ const routes: Routes = [
     ]
   },
   {
-    path: "quizzes", canActivate: [() => checkAccess(AccessRestriction.User)], children: [
+    path: "quizzes", children: [
       {path: "", pathMatch: "full", component: QuizGameSelectionComponent},
       {path: "quiz/:quiz_id", component: QuizGameComponent}
     ]
